c8cf64388b9549e261f5917db7adb93d4ced7a9a
“Alin <androidphone934@gmail.comm>
2021-04-11 10:55:23 +0300

    Tema Lab10

---
+++ src/Laborator2/Animal_Rescue/Girl.java
@@ -0,0 +1,19 @@
+package Laborator2.Animal_Rescue;
+
+public class Girl {
+
+    String breed = "Human";
+    String color = "White caucasian";
+    String name = "Amy";
+    String hairColor = "brown";
+    String eyesColor = "blue";
+    int age = 8;
+    int heightinCm = 120;
+    int weightinKg =  25;
+    int Funds =10;
+
+    }
+
+
+
+
---
+++ src/Laborator2/Animal_Rescue/Veterinary.java
@@ -0,0 +1,8 @@
+package Laborator2.Animal_Rescue;
+
+public class Veterinary {
+
+    String Name = "Frank";
+
+    String Doc_Type= "Dog Veterinary";
+}
